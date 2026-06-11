356eaec731563bae31b1f59b064d1060d8152512
Rizky Mahendra <rizkymh31@gmail.com>
2026-06-11 20:31:26 +0700

    submit hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,21 @@
 package hamming
 
+import fmt "fmt"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	
+	if len(a) != len(b) {
+		return 0, fmt.Errorf("strings must be of equal length")
+	}
+
+	distance :=0
+
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
+
 }
